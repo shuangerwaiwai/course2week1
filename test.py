885118c1dd99937e3f5be3a18b54a485da883a1f
wangyanfei <1136949416@qq.com>
2020-06-19 09:19:23 +0800

    use L2 regularization

--- test.py
+++ test.py
@@ -214,5 +214,53 @@ def model(X, Y, learning_rate=0.3, num_iterations=30000, print_cost=True, is_plo
 # axes.set_ylim([-0.75, 0.65])
 # reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
 
+def compute_cost_with_regularization(A3, Y, parameters, lambd):
+    m = Y.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    W3 = parameters["W3"]
 
+    cross_entropy_cost = reg_utils.compute_cost(A3, Y)
+    L2_regularization_cost = lambd * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3)))/(2*m)
 
+    cost = cross_entropy_cost + L2_regularization_cost
+
+    return cost
+
+def backward_propagation_with_regularization(X, Y, cache, lambd):
+
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+
+    dW3 = (1/m) * np.dot(dZ3, A2.T) + ((lambd * W3)/m)
+    db3 = (1/m) * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2>0))
+    dW2 = (1/m) * np.dot(dZ2, A1.T) + ((lambd * W2)/m)
+    db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1>0))
+    dW1 = (1/m) * np.dot(dZ1, X.T) + ((lambd * W1)/m)
+    db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1,
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+parameters = model(train_X, train_Y, lambd=0.7, is_plot=True)
+print("train_set:")
+predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+print("test set:")
+predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+
+plt.title("Model with L2-regularization")
+axes = plt.gca()
+axes.set_xlim([-0.75, 0.40])
+axes.set_ylim([-0.75, 0.65])
+reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
