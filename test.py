ed94b4120c7d1d1910955ba49fd9ce9cbbd9d290
wangyanfei <1136949416@qq.com>
2020-06-19 08:52:39 +0800

    not use regularization

--- test.py
+++ test.py
@@ -140,14 +140,79 @@ def initialize_parameters_he(layers_dims):
 # print("W2=" + str(parameters["W2"]))
 # print("b2=" + str(parameters["b2"]))
 
-parameters = model(train_X, train_Y, initialization="he", is_plot=True)
-print("train_set:")
-prediction_train = init_utils.predict(train_X, train_Y, parameters)
-print("test_set:")
-prediction_test = init_utils.predict(test_X, test_Y, parameters)
+# parameters = model(train_X, train_Y, initialization="he", is_plot=True)
+# print("train_set:")
+# prediction_train = init_utils.predict(train_X, train_Y, parameters)
+# print("test_set:")
+# prediction_test = init_utils.predict(test_X, test_Y, parameters)
+#
+# plt.title("Model with He initialization")
+# axes = plt.gca()
+# axes.set_xlim([-1.5, 1.5])
+# axes.set_ylim([-1.5, 1.5])
+# init_utils.plot_decision_boundary(lambda x: init_utils.predict_dec(parameters, x.T), train_X, train_Y)
 
-plt.title("Model with He initialization")
-axes = plt.gca()
-axes.set_xlim([-1.5, 1.5])
-axes.set_ylim([-1.5, 1.5])
-init_utils.plot_decision_boundary(lambda x: init_utils.predict_dec(parameters, x.T), train_X, train_Y)
+
+train_X, train_Y, test_X, test_Y = reg_utils.load_2D_dataset(is_plot=True)
+
+def model(X, Y, learning_rate=0.3, num_iterations=30000, print_cost=True, is_plot = True, lambd=0, keep_prob=1):
+    grads={}
+    costs=[]
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 20, 3, 1]
+
+    parameters = reg_utils.initialize_parameters(layers_dims)
+
+    for i in range(0, num_iterations):
+        if keep_prob == 1:
+            a3, cache = reg_utils.forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+        else:
+            print("keep_prob error! exit!")
+            exit
+
+        if lambd == 0:
+            cost = reg_utils.compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+
+        assert(lambd == 0 or keep_prob == 1)
+
+        if (lambd == 0 and keep_prob == 1):
+            grads = reg_utils.backward_propagation(X, Y, cache)
+        elif lambd!=0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+        elif keep_prob<1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+
+        parameters = reg_utils.update_parameters(parameters, grads, learning_rate)
+
+        if i % 1000 == 0:
+            costs.append(cost)
+            if(print_cost and i % 10000 == 0):
+                print(str(i) + "th iteration, cost is:" + str(cost))
+
+    if is_plot:
+        plt.plot(costs)
+        plt.ylabel("cost")
+        plt.xlabel("iteration (x1,000)")
+        plt.title("Learning rate = "+ str(learning_rate))
+        plt.show()
+
+    return parameters
+
+# parameters = model(train_X, train_Y, is_plot=True)
+# print("train set:")
+# predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+# print("test set:")
+# predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+#
+# plt.title("Model without regularization")
+# axes = plt.gca()
+# axes.set_xlim([-0.75, 0.40])
+# axes.set_ylim([-0.75, 0.65])
+# reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
