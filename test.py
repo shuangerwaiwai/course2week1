62f970711634eedc4df7e0545e3db90a0bc43036
wangyanfei <1136949416@qq.com>
2020-06-23 09:35:34 +0800

    tidu test

--- test.py
+++ test.py
@@ -325,14 +325,94 @@ def backward_propagation_with_dropout(X, Y, cache, keep_prob):
 
     return gradients
 
-parameters = model(train_X, train_Y, keep_prob=0.86, learning_rate=0.3, is_plot=True)
-print("dropout, train_set:")
-predictions_train = reg_utils.predict(train_X, train_Y, parameters)
-print("dropout, test_set:")
-predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+# parameters = model(train_X, train_Y, keep_prob=0.86, learning_rate=0.3, is_plot=True)
+# print("dropout, train_set:")
+# predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+# print("dropout, test_set:")
+# predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+#
+# plt.title("Model with dropout")
+# axes = plt.gca()
+# axes.set_xlim([-0.75, 0.40])
+# axes.set_ylim([-0.75, 0.65])
+# reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
 
-plt.title("Model with dropout")
-axes = plt.gca()
-axes.set_xlim([-0.75, 0.40])
-axes.set_ylim([-0.75, 0.65])
-reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
+def forward_propagation_n(X, Y, parameters):
+    m = X.shape[1]
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = gc_utils.relu(Z1)
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = gc_utils.relu(Z2)
+
+    Z3 = np.dot(W3, A2) + b3
+    A3 = gc_utils.sigmoid(Z3)
+
+    logprobs = np.multiply(-np.log(A3), Y) + np.multiply(-np.log(1-A3), 1-Y)
+    cost = (1/m) * np.sum(logprobs)
+
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return cost, cache
+
+def backward_propagation_n(X, Y, cache):
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = (1./m) * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2>0))
+    dW2 = 1./m*np.dot(dZ2, A1.T)
+    db2 = 1./m*np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1>0))
+    dW1 = 1./m*np.dot(dZ1, X.T)
+    db1 = 1./m*np.sum(dZ1, axis=1, keepdims=True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                 "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                 "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7):
+    parameters_value, keys = gc_utils.dictionary_to_vector(parameters)
+    grad = gc_utils.gradients_to_vector(gradients)
+    num_parameters = parameters_value.shape[0]
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+
+    for i in range(num_parameters):
+        thetaplus = np.copy(parameters_value)
+        thetaplus[i][0] = thetaplus[i][0] + epsilon
+        J_plus[i], cache = forward_propagation_n(X, Y, gc_utils.vector_to_dictionary(thetaplus))
+
+        thetaminus = np.copy(parameters_value)
+        thetaminus[i][0] = thetaminus[i][0] - epsilon
+        J_minus[i], cache = forward_propagation_n(X, Y, gc_utils.vector_to_dictionary(thetaminus))
+
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+
+    numerator = np.linalg.norm(grad - gradapprox)
+    denominator = np.linalg.norm(grad - gradapprox)
+    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)
+    difference = numerator / denominator
+
+    if difference < 1e-7:
+        print("yes!")
+    else:
+        print("No!")
+
+    return difference
